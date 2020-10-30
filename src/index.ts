047696193b545b3805edd1ab41cdbb906152c365
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-30 17:20:55 +0000

    Part 3: Add dictionary based counter

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,34 @@ function readSampleText(): string {
 function getTrigramsFromOffset(input: string, offset: number): string[] {
     // Chunking using regex, using dotAll flag (s) so all whitespace characters get
     // treated like individual characters in the trigram
-    return input.substring(offset).match(/.{1,3}/gs) ?? [];
+    return input.substring(offset).match(/.{3}/gs) ?? [];
 }
 
 function getAllTrigrams(input: string): string[] {
     // Make list of all trigrams in input by splitting into chunks at offsets 0, 1, and 2
-    return _.flatMap(_.range(0, 3), i => getTrigramsFromOffset(input, i));
+    return _.flatMap(_.range(0, 3), i => getTrigramsFromOffset(input, i))
+        .filter(s => /[a-z]{3}/.test(s));
 }
 
 function naiveTls(input: string): number {
     return getAllTrigrams(input).filter(s => /tra/i.test(s)).length;
 }
 
-console.log(naiveTls(readSampleText()));
+function dictionaryTls(input: string): _.Dictionary<number> {
+    const trigrams = getAllTrigrams(input.toLowerCase());
+
+    const dictionary: _.Dictionary<number> = {};
+    trigrams.map(trigram =>
+        dictionary[trigram] = (dictionary[trigram] ?? 0) + 1
+    );
+
+    return dictionary;
+}
+
+function getEntriesWithValue(dictionary: _.Dictionary<number>, target: number): string[] {
+    return Object.entries(dictionary)
+        .filter(([, value]) => value === target)
+        .map(([key]) => key);
+}
+
+console.log(getEntriesWithValue(dictionaryTls(readSampleText()), 63));
