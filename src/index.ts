11b8655c65dcf2a80135c25c43bb6472d44dd764
Benji Marshall <Benji.Marshall@softwire.com>
2020-10-30 14:58:07 +0000

    Naive solution for part 1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
-function helloWorld(): void {
-    console.log('Hello world');
+import * as fs from "fs";
+
+const sampleTextPath = "data/SampleText.txt";
+
+function readSampleText(): string {
+    return fs.readFileSync(sampleTextPath, "utf8");
 }
 
-helloWorld();
+function naiveTls(input: string): number {
+    const inputArray = [...input];
+
+    let counter = 0;
+    for (let i = 0; i < inputArray.length - 2; i++) {
+        if (inputArray[i].toLowerCase() === "t"
+            && inputArray[i + 1].toLowerCase() === "r"
+            && inputArray[i + 2].toLowerCase() === "a") {
+
+            counter++;
+        }
+    }
+
+    return counter;
+}
+
+console.log(naiveTls(readSampleText()));
