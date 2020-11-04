66c362fcd3e4b3a92219eee3740e1c0891ef914d
Benji Marshall <Benji.Marshall@softwire.com>
2020-11-04 16:12:35 +0000

    Add tests for the project

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
 import * as fs from 'fs';
 import * as _ from 'lodash';
 
-function readSampleText(): string {
+export function readSampleText(): string {
     const sampleTextPath = 'data/SampleText.txt';
     return fs.readFileSync(sampleTextPath, 'utf8');
 }
 
-function getTrigramsFromOffset(input: string, offset: number): string[] {
+export function getTrigramsFromOffset(input: string, offset: number): string[] {
     // Chunking using regex, using dotAll flag (s) so all whitespace characters get
     // treated like individual characters in the trigram
     return input.substring(offset).match(/.{3}/gs) ?? [];
 }
 
-function getAllTrigrams(input: string): string[] {
+export function getAllTrigrams(input: string): string[] {
     // Make list of all trigrams in input by splitting into chunks at offsets 0, 1, and 2
     return _.flatMap(_.range(0, 3), i => getTrigramsFromOffset(input, i));
 }
 
-function getAllThreeLetterStrings(input: string): string[] {
+export function getAllThreeLetterStrings(input: string): string[] {
     return getAllTrigrams(input.toLowerCase()).filter(s => /[a-z]{3}/.test(s));
 }
 
-function naiveTls(input: string): number {
+export function naiveTls(input: string): number {
     return getAllTrigrams(input).filter(s => /tra/i.test(s)).length;
 }
 
-function dictionaryTls(input: string): { [trigram: string]: number; } {
+export function dictionaryTls(input: string): { [trigram: string]: number; } {
     return _.countBy(getAllThreeLetterStrings(input));
 }
 
-function getEntriesWithValue(
+export function getEntriesWithValue(
     dictionary: { [trigram: string]: number; },
     target: number
 ): string[] {
@@ -38,5 +38,3 @@ function getEntriesWithValue(
         .keys()
         .value();
 }
-
-console.log(getEntriesWithValue(dictionaryTls(readSampleText()), 63));
